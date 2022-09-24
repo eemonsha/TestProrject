8d49316b8abd2ffd530e02c4cbea52733de19c85
MD Ileas Sha Emon <emonshah789@gmail.com>
2022-09-25 01:36:22 +0600

    kj

---
+++ TestProrject/Controllers/StudentsController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NToastNotify;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestProrject.Data;
+using TestProrject.ViewModel;
+
+namespace TestProrject.Controllers
+{
+    public class StudentsController : Controller
+    {
+
+        private readonly DataContext _context;
+        private readonly IToastNotification _toastNotification;
+        private readonly IWebHostEnvironment webHostEnvironment;
+
+        public StudentsController(DataContext context, IToastNotification toastNotification , IWebHostEnvironment hostEnvironment)
+        {
+            _context = context;
+            _toastNotification = toastNotification;
+            webHostEnvironment = hostEnvironment;
+
+        }
+        public IActionResult StudentIndex()
+        {
+            return View();
+        }
+
+        public IActionResult StudentCreate()
+        {
+            IEnumerable<SelectListItem> dep = from Department in _context.Departments.ToList()
+                                              select new SelectListItem
+                                              {
+                                                  Value = Department.DepartmentID.ToString(),
+                                                  Text = Department.DepartmentName
+                                              };
+            ViewBag.deepart = dep;
+
+
+
+            var NewStudent = new StudentVM();
+            NewStudent.Balance = 0;
+            NewStudent.StudentScore = 0;
+            return View(NewStudent);
+        }
+
+        //[HttpPost]
+        //public IActionResult StudentCreate()
+        //{
+        //    return View();
+        //}
+    }
+}
---
+++ TestProrject/ViewModel/StudentVM.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestProrject.ViewModel
+{
+    public class StudentVM
+    {
+
+
+        public int StudentID { get; set; }
+        public String StudentName { get; set; }
+        public String StudentIDentity { get; set; }
+        public int DepID { get; set; }
+        public decimal Balance { get; set; }
+        public decimal RemainBalance { get; set; }
+        //public String PPicture { get; set; }
+        public bool ISActive { get; set; }
+        public int StudentScore { get; set; }
+        public String StudentStatus { get; set; }
+
+
+        //Image Add
+        public IFormFile StudentImage { get; set; }
+
+        public bool DeleteStu { get; set; }
+    }
+}
